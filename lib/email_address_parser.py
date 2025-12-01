fc93f9b59e3ebf6edc235d4830f4cd28c079f92c
PKMDroid <princekibali222@gmail.com>
2025-12-01 07:33:55 +0300

    updates

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,10 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, addresses):
+        self.addresses = addresses
+
+    def parse(self):
+        emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}", self.addresses)
+        return sorted(set(emails))
